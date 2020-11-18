4eb34979601f56dd52ffc5c8bb444c67d3f1a8ee
sammy310 <kipper098@gmail.com>
2020-11-18 20:31:29 +0900

    파일 저장 기능 구현

--- Cogs/Drive.py
+++ Cogs/Drive.py
@@ -41,6 +41,49 @@ class Drive(commands.Cog, name='Drive'):
             if not os.path.exists(driveDir):
                 os.mkdir(driveDir)
             
+            for file in message.attachments:
+                fName = '.'.join(file.filename.split('.')[:-1])
+                fExtension = file.filename.split('.')[-1]
+                fileDupCount = 0
+                fileName = file.filename
+                
+                while True:
+                    if not os.path.exists(f'{driveDir}/{fileName}'):
+                        r = requests.get(file.url, allow_redirects=True)
+                        with open(f'{driveDir}/{fileName}', 'wb') as f:
+                            f.write(r.content)
+                            
+                        # Save Data
+                        dataDict1 = { fileName: { 'originalName': file.filename, 'url': file.url } }
+                        dataDict2 = { fileName: { 'originalName': file.filename, 'path': f'{driveDir}/{fileName}', 'url': file.url } }
+                        
+                        fDataPath = f'{drivePath}/{DRIVE_FILE_ALL_DATA}'
+                        if not os.path.exists(fDataPath):
+                            with open(fDataPath, 'w', -1, 'utf-8') as f:
+                                json.dump(dataDict2, f, indent=4, sort_keys=True)
+                        else:
+                            with open(fDataPath, 'r+', -1, 'utf-8') as f:
+                                data = json.load(f)
+                                data.update(dataDict2)
+                                f.seek(0)
+                                json.dump(data, f, indent=4, sort_keys=True)
+                        
+                        fDataPath = f'{driveDir}/{DRIVE_FILE_DATA}'
+                        if not os.path.exists(fDataPath):
+                            with open(fDataPath, 'w', -1, 'utf-8') as f:
+                                json.dump(dataDict1, f, indent=4, sort_keys=True)
+                        else:
+                            with open(fDataPath, 'r+', -1, 'utf-8') as f:
+                                data = json.load(f)
+                                data.update(dataDict1)
+                                f.seek(0)
+                                json.dump(data, f, indent=4, sort_keys=True)
+                            
+                        break
+                    else:
+                        fileDupCount += 1
+                        fileName = f'{fName}{fileDupCount}.{fExtension}'
+                        
 
 def setup(bot):
     bot.add_cog(Drive(bot))
