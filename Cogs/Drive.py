61f65a03988624fe5ac7b44fbd8089d2e7e8da47
sammy310 <kipper098@gmail.com>
2020-11-19 15:42:53 +0900

    저장된 파일 불러오는 기능 추가

--- Cogs/Drive.py
+++ Cogs/Drive.py
@@ -3,10 +3,11 @@
 
 import os
 import datetime
+import requests
 import json
 import discord
 from discord.ext import commands
-
+import asyncio
 
 DATA_PATH = 'Data'
 DRIVE_PATH = 'Drive'
@@ -84,6 +85,51 @@ class Drive(commands.Cog, name='Drive'):
                         fileDupCount += 1
                         fileName = f'{fName}{fileDupCount}.{fExtension}'
                         
+    @commands.command(name='파일')
+    async def getFiles(self, ctx):
+        drivePath = f'{DATA_PATH}/{ctx.message.guild.id}/{DRIVE_PATH}'
+        dataPath = f'{drivePath}/{DRIVE_FILE_ALL_DATA}'
+        if not os.path.exists(dataPath):
+            await ctx.send('저장된 파일이 없습니다!!')
+            return
+            
+        with open(dataPath, 'r', -1, 'utf-8') as f:
+            data = json.load(f)
+            
+        dataStr = '0. 취소\n'
+        dataList = []
+        for i, t in enumerate(data):
+            dataList.append(t)
+            dataStr += f'{i+1}. {t}\n'
+            
+        embed = discord.Embed(title='원하는 파일을 선택하세요', description=dataStr, color=0x147AF3)
+        message = await ctx.send(embed=embed)
+        
+        selectIndex = 0
+        while True:
+            try:
+                msg = await ctx.bot.wait_for('message', timeout=60.0)
+            except asyncio.TimeoutError:
+                await message.edit(content='시간 초과!!')
+                return
+            else:
+                if ctx.author == msg.author and msg.content.isdigit():
+                    selectIndex = int(msg.content)
+                    if selectIndex == 0:
+                        await msg.delete()
+                        await message.edit(content = '취소되었습니다')
+                        return
+                    elif selectIndex > 0 and selectIndex <= len(dataList):
+                        await msg.delete()
+                        break
+                    else:
+                        await msg.delete()
+                        await message.edit(content = '올바른 값을 입력해주세요!!')
+        
+        await message.delete()
+        
+        await ctx.send(data[dataList[selectIndex-1]]['url'])
+
 
 def setup(bot):
     bot.add_cog(Drive(bot))
