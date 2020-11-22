20a53b986be96dc3afe6f2504c650516df2ac089
RomuRaymu <ghy2213@naver.com>
2020-11-23 00:16:44 +0900

    .

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ for filename in os.listdir('Cogs'):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name='과제'))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name='( ͡° ͜ʖ ͡°)'))
         
 
     
